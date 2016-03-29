9aed688f9838a13e6682cb01afc350581d073071
EricKao <a6668r@hotmail.com>
2016-03-29 11:22:44 +0800

    [List Comprehensions] Add list comprehensions source file.

---
+++ 7_ListComprehensions.py
@@ -0,0 +1,6 @@
+__author__ = "Eric"
+
+List = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Make a new list that only has the even number in it and print out in one line of Python
+print [element for element in List if element % 2 == 0]
