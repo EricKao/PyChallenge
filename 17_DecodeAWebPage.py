df9c6a00f0aff7900fcce35abcc503949bef2183
EricKao <a6668r@hotmail.com>
2016-04-01 17:54:41 +0800

    [PyChallenge] Refine to use main function.

--- 17_DecodeAWebPage.py
+++ 17_DecodeAWebPage.py
@@ -5,16 +5,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Using requests module to get url request
-Url = 'http://www.nytimes.com'
-r = requests.get(Url)
 
-# Using BeautifulSoup to parse response html
-Soup = BeautifulSoup(r.text, "html.parser")
+def main():
+    # Using requests module to get url request
+    Url = 'http://www.nytimes.com'
+    r = requests.get(Url)
 
-# Print out all story-heading line text
-for story_heading in Soup.find_all(class_="story-heading"):
-    if story_heading.a:
-        print (story_heading.a.text.encode("utf-8").replace("\n", " ").strip())
-    else:
-        print (story_heading.contents[0].encode("utf-8").strip())
+    # Using BeautifulSoup to parse response html
+    Soup = BeautifulSoup(r.text, "html.parser")
+
+    # Print out all story-heading line text
+    for story_heading in Soup.find_all(class_="story-heading"):
+        if story_heading.a:
+            print (story_heading.a.text.encode("utf-8").replace("\n", " ").strip())
+        else:
+            print (story_heading.contents[0].encode("utf-8").strip())
+
+if __name__ == "__main__":
+    main()
